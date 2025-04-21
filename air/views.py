dac0afc9c65492351f75f5aae70b1bb323fa9bde
Sai Khun Main <saikhunmain.s@ku.th>
2025-04-21 14:59:39 +0700

    feat(frontend): implement chart layout refactor

--- air/views.py
+++ air/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 from .models import Projects
 
 def home(request):
-    data = Projects.objects.all().order_by('-id')[:10]
-    return render(request, 'home.html', {'data': data})
-# Create your views here.
+    all_data = Projects.objects.order_by('-ts')[:20]
+    latest = all_data[0]
+
+    timestamps = [entry.ts.strftime("%Y-%m-%d %H:%M") for entry in reversed(all_data)]
+    pm_values = [entry.pm2_5 for entry in reversed(all_data)]
+    pm10_values = [entry.pm10 for entry in reversed(all_data)]
+    humidity_values = [entry.humidity for entry in reversed(all_data)]
+    temperature_values = [entry.temp for entry in reversed(all_data)]
+
+    return render(request, 'home.html', {
+        'latest': latest,
+        'timestamps': timestamps,
+        'pm_values': pm_values,
+        'pm10_values': pm10_values,
+        'humidity_values': humidity_values,
+        'temperature_values': temperature_values
+    })
